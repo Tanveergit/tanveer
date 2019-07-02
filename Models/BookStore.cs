8d0a2b521cbb323ab12bb9f53d1fe4d8fa195d98
tanveerahmed <tanveerahamed11@gmail.com>
2019-07-02 17:00:10 +0530

    initial commit

---
+++ Models/BookStore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace WebAPIRc.Models
+{
+    public class BookStore : DbContext
+    {
+        public BookStore() : base("DefaultConnection")
+        {
+
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+}
